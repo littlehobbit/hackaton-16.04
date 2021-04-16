3d1f8abea3fbd371d5ec47f418b3476b0c27f9f1
StadndartAccount <72969531+StadndartAccount@users.noreply.github.com>
2021-04-16 16:05:32 +0300

    Питоновская виртуалОчка

---
+++ back-part/voice-processing/main.py
@@ -0,0 +1,12 @@
+import json
+import speech_recognition
+
+def get_emotions(message):
+    return True
+
+def audioToText():
+    sample_audio = speech_recognition.AudioFile('E:/Datasets/my_audio.wav')
+
+
+print(get_emotions("After living abroad for such a long time, seeing my family was the best present I could have ever wished for."))
+
